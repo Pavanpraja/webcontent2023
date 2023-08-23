e0e266c9fb7acb94869c9baa9a45710bfea725a9
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2023-08-23 19:36:37 +0530

    Commited all files

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+const users = [];
+
+//using middlewares
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
+
+//use view engin of ejs
+// app.set("view engine", "ejs");
+
+app.use("/",express.static('images'));
+app.use("/FAQ",express.static('images'));
+app.use("/Projects",express.static('images'));
+app.use("/Tos",express.static('images'));
+
+app.get("/", (req, res)=>{
+    res.render("index");
+})
+app.get("/FAQ", (req, res)=>{
+    res.render("FAQ.html");
+})
+app.get("/projects.html", (req, res)=>{
+    res.render("project");
+})
+app.get("/Tos.html", (req, res)=>{
+    res.render("TOS");
+})
+
+app.post("/contact", (req, res)=>{
+    console.log(req.body.name);
+    console.log(req.body.email);
+
+    users.push({username:req.body.name, email:req.body.email});
+    res.send("Message send successfully");
+    // res.redirect("/post");
+});
+
+app.get("/users", (req, res)=>{
+    res.json({
+        users,
+    })
+})
+
+app.listen(5500, ()=>{
+    console.log('Server is working');
+})
