3c51c93a0f3bf9ca6bb76569e694daf2ad59ddc0
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2023-08-24 20:03:48 +0530

    Files updated

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose'; //Mongoose material
+import express from "express";
+import path from "path";
+import mongoose from "mongoose"; //Mongoose material
 
-mongoose.connect("mongodb://localhost:27017", { //Mongoose material
-    dbName: "WEBDATA" //Mongoose material
-}).then(()=>console.log("Database have been connected")).catch((e)=>console.log(e)); //Mongoose material
+mongoose
+  .connect("mongodb://localhost:27017", {
+    //Mongoose material
+    dbName: "WEBDATA", //Mongoose material
+  })
+  .then(() => console.log("Database have been connected"))
+  .catch((e) => console.log(e)); //Mongoose material
 
-const messageSchema = new mongoose.Schema({ //Mongoose material
-    name: String, //Mongoose material
-    email: String, //Mongoose material
+const messageSchema = new mongoose.Schema({
+  //Mongoose material
+  name: String, //Mongoose material
+  email: String, //Mongoose material
 }); //Mongoose material
 
-const Message = mongoose.model("Message", messageSchema) //Mongoose material
+const Message = mongoose.model("Message", messageSchema); //Mongoose material
 
 const app = express();
 
@@ -24,39 +29,44 @@ app.use(express.urlencoded({ extended: true }));
 //use view engin of ejs
 // app.set("view engine", "ejs");
 
-app.use("/",express.static('images'));
-app.use("/FAQ",express.static('images'));
-app.use("/Projects",express.static('images'));
-app.use("/Tos",express.static('images'));
+app.use("/", express.static("images"));
+app.use("/FAQ", express.static("images"));
+app.use("/Projects", express.static("images"));
+app.use("/Tos", express.static("images"));
 
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.render("index");
 
-app.get("/", (req, res)=>{
-    res.render("index");
-})
-app.get("/FAQ", (req, res)=>{
-    res.render("FAQ");
-})
-app.get("/Projects", (req, res)=>{
-    res.render("project");
-})
-app.get("/Tos", (req, res)=>{
-    res.render("TOS");
-})
+  console.log(req.cookies);
+});
+app.get("/FAQ", (req, res) => {
+  res.render("FAQ");
+});
+app.get("/Projects", (req, res) => {
+  res.render("project");
+});
+app.get("/Tos", (req, res) => {
+  res.render("TOS");
+});
 
 //start Authentication
-app.post("/login",(req, res)=>{
-    
-})
-app.post("/contact", (req, res)=>{
-    console.log(req.body.name);
-    console.log(req.body.email);
+app.post("/Signup", (req, res) => {
+  res.cookie("token", "imin", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+  res.redirect("/");
+});
+app.post("/contact", (req, res) => {
+  console.log(req.body.name);
+  console.log(req.body.email);
 
-    Message.create({ name: req.body.name, email:req.body.email }); //Mongoose material
+  Message.create({ name: req.body.name, email: req.body.email }); //Mongoose material
 
-    res.render("Success");
-    // res.redirect("/post");
+  res.render("Success");
+  // res.redirect("/post");
 });
 
 // app.get("/users", (req, res)=>{
@@ -65,6 +75,6 @@ app.post("/contact", (req, res)=>{
 //     })
 // })
 
-app.listen(5500, ()=>{
-    console.log('Server is working');
-})
+app.listen(5500, () => {
+  console.log("Server is working");
+});
