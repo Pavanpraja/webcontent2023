c81ab94ccad1f5e43adb08b86c9d95ad5c1976a8
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2023-08-24 19:39:21 +0530

    Files updated

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose'; //Mongoose material
+
+mongoose.connect("mongodb://localhost:27017", { //Mongoose material
+    dbName: "WEBDATA" //Mongoose material
+}).then(()=>console.log("Database have been connected")).catch((e)=>console.log(e)); //Mongoose material
+
+const messageSchema = new mongoose.Schema({ //Mongoose material
+    name: String, //Mongoose material
+    email: String, //Mongoose material
+}); //Mongoose material
+
+const Message = mongoose.model("Message", messageSchema) //Mongoose material
 
 const app = express();
 
-const users = [];
+// const users = [];
 
 //using middlewares
 app.use(express.static(path.join(path.resolve(), "public")));
@@ -19,9 +31,6 @@ app.use("/Tos",express.static('images'));
 
 app.set("view engine", "ejs");
 
-app.get("/add", (req, res)=>{
-    res.send("Created this page");
-})
 
 app.get("/", (req, res)=>{
     res.render("index");
@@ -36,20 +45,25 @@ app.get("/Tos", (req, res)=>{
     res.render("TOS");
 })
 
+//start Authentication
+app.post("/login",(req, res)=>{
+    
+})
 app.post("/contact", (req, res)=>{
     console.log(req.body.name);
     console.log(req.body.email);
 
-    users.push({username:req.body.name, email:req.body.email});
+    Message.create({ name: req.body.name, email:req.body.email }); //Mongoose material
+
     res.render("Success");
     // res.redirect("/post");
 });
 
-app.get("/users", (req, res)=>{
-    res.json({
-        users,
-    })
-})
+// app.get("/users", (req, res)=>{
+//     res.json({
+//         users,
+//     })
+// })
 
 app.listen(5500, ()=>{
     console.log('Server is working');
