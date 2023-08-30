ce433880a21581de20028092dd90d6c60fe9a557
Pavan <129081053+Pavanpraja@users.noreply.github.com>
2023-08-30 23:26:39 +0530

    files has been commited

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose"; //Mongoose material
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
-mongoose.connect("mongodb://localhost:27017", {
+mongoose
+  .connect("mongodb://localhost:27017", {
     //Mongoose material
     dbName: "WEBDATA", //Mongoose material
   })
@@ -16,8 +18,14 @@ const messageSchema = new mongoose.Schema({
   email: String, //Mongoose material
 }); //Mongoose material
 
+const userSche = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
 
 const Message = mongoose.model("Message", messageSchema); //Mongoose material
+const userMess = mongoose.model("user", userSche);
 
 const app = express();
 
@@ -38,24 +46,32 @@ app.use("/Tos", express.static("images"));
 
 app.set("view engine", "ejs");
 
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
   console.log(token);
 
   if (token) {
+    const decoded = jwt.verify(token, "lajfjljafjldfa");
+
+    req.userMess = await userMess.findById(decoded._id);
+
     next();
   } else {
-    res.render("Signup");
+    res.redirect("/Signup");
   }
 };
+
+
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/index", isAuthenticated, (req, res) => {
+  console.log(req.userMess);
   res.render("Signout");
 });
+
 
 app.get("/FAQ", (req, res) => {
   res.render("FAQ");
@@ -70,19 +86,66 @@ app.get("/Signup", (req, res) => {
   res.render("Signup");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 //start Authentication
 
-app.post("/Signup", (req, res) => {
-  
-  res.cookie("token", "imin", {
+app.post("/Signup", async (req, res) => {
+  console.log(req.body);
+
+  const { email, password } = req.body;
+
+  let user = await userMess.findOne({ email });
+
+  if (!user) {
+    return res.redirect("/register");
+  }
+
+  const isMatch = user.password === password;
+
+  if(!isMatch){
+    return res.render("Signup", {message: "Incorrect password"});
+  }
+
+  // user = await userMess.create({ email, password });
+
+  const token = jwt.sign({ _id: user._id }, "lajfjljafjldfa");
+  console.log(token);
+
+  res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + 60 * 1000),
   });
   res.redirect("/index");
 });
 
+app.post("/register", async (req, res)=>{
+  console.log(req.body);
+
+  const {name, email, password} = req.body;
+
+  let user = await userMess.findOne({email});
+
+  if(user) {
+    return redirect("/Signup");
+  }
+
+
+  user = await userMess.create({name, email, password });
+
+  const token = jwt.sign({_id:user._id}, "lajfjljafjldfa");
+  console.log(token);
+
+  res.cookie("token", token,{
+    httpOnly: true,
+    expires: new Date(Date.now() + 60*1000),
+  });
+});
+
+
 app.get("/Signout", (req, res) => {
-  
   res.cookie("token", null, {
     expires: new Date(Date.now()),
   });
@@ -98,8 +161,6 @@ app.post("/contact", (req, res) => {
   res.render("Success");
   // res.redirect("/post");
 });
-
-
 
 // app.get("/users", (req, res)=>{
 //     res.json({
